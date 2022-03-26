1bbf45d52a9e2478d730298c32ca84eef17737c2
Ahmad Nur Fauzi <ahmadnurfauzi1704@gmail.com>
2022-03-26 15:11:15 +0700

    Source Code Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum3_Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Fauzi";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 17;
+            
+            // pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
